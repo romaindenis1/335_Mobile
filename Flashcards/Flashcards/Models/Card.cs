5e5e911d2a3e0e198b5af0e88608668520f0ad19
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-05-09 10:12:35 +0200

    Create Card.cs

---
+++ Flashcards/Flashcards/Models/Card.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Flashcards
+{
+    public class Card : INotifyPropertyChanged
+    {
+        private string _title;
+        private string _description;
+
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Description
+        {
+            get => _description;
+            set
+            {
+                if (_description != value)
+                {
+                    _description = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        //why does this work bruh
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
