eaeb9940e8edbdce65250076a9470e529b4e5725
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-05-16 09:04:02 +0200

    Create(Animations Project)

---
+++ Exercises/Animations/Animations/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿namespace Animations
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var vm = new Animate2ViewModel();
+            BindingContext = vm;
+
+            vm.RotateBoxUIAction = RotateIt;
+            vm.MoveBoxUIAction = MoveBox;
+        }
+
+        private void RotateIt(int angle)
+        {
+            if (this.box != null)
+                this.box.RotateTo(angle);
+        }
+
+        private void MoveBox(int x)
+        {
+            if (this.box != null)
+                this.box.TranslationX += x;
+        }
+    }
+
+
+
+}
---
+++ Exercises/Animations/Animations/ViewModels/Animate2ViewModels.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Diagnostics;
+
+public partial class Animate2ViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private bool toggled = false;
+
+    [ObservableProperty]
+    private string label = "Rotate 90°";
+
+    [ObservableProperty]
+    private int speed = 1;
+
+    public Action<int>? RotateBoxUIAction { get; set; }
+    public Action<int>? MoveBoxUIAction { get; set; }
+
+    partial void OnToggledChanged(bool value)
+    {
+        RotateBox(value ? 90 : 0);
+        Label = value ? "Rotate back" : "Rotate 90°";
+    }
+
+    private void RotateBox(int angle)
+    {
+        if (RotateBoxUIAction != null)
+        {
+            RotateBoxUIAction.Invoke(angle);
+        }
+        else
+        {
+            Trace.WriteLine($"No rotation action defined, would rotate {angle}");
+        }
+    }
+
+    [RelayCommand]
+    private void MoveBox(int multiplier)
+    {
+        if (MoveBoxUIAction != null)
+        {
+            MoveBoxUIAction.Invoke(Speed * multiplier);
+        }
+        else
+        {
+            Trace.WriteLine($"No move action defined, would move x {Speed * multiplier}");
+        }
+    }
+}
