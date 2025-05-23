7371958e054c3efe163fced388fae26f7946c67a
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-05-23 10:19:08 +0200

    Update ResultsPage.xaml.cs

--- Flashcards/Flashcards/ResultsPage.xaml.cs
+++ Flashcards/Flashcards/ResultsPage.xaml.cs
@@ -1,9 +1,41 @@
+using System.Diagnostics;
+
 namespace Flashcards;
 
 public partial class ResultsPage : ContentPage
 {
-	public ResultsPage()
+	public ResultsPage(int correctAnswers, int totalCards, TimeSpan timeSpent)
 	{
-		InitializeComponent();
+		Debug.WriteLine($"ResultsPage constructor called with: CorrectAnswers={correctAnswers}, TotalCards={totalCards}, TimeSpent={timeSpent}");
+		
+		try
+		{
+			InitializeComponent();
+			Debug.WriteLine("InitializeComponent completed");
+			
+			var viewModel = new ResultsViewModel(correctAnswers, totalCards, timeSpent);
+			Debug.WriteLine("ResultsViewModel created");
+			
+			BindingContext = viewModel;
+			Debug.WriteLine($"BindingContext set to ResultsViewModel. CorrectAnswers={viewModel.CorrectAnswers}, TotalCards={viewModel.TotalCards}");
+		}
+		catch (Exception ex)
+		{
+			Debug.WriteLine($"Error in ResultsPage constructor: {ex}");
+		}
+	}
+
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		Debug.WriteLine("ResultsPage OnAppearing called");
+		if (BindingContext is ResultsViewModel viewModel)
+		{
+			Debug.WriteLine($"OnAppearing - ViewModel values: CorrectAnswers={viewModel.CorrectAnswers}, TotalCards={viewModel.TotalCards}");
+		}
+		else
+		{
+			Debug.WriteLine("OnAppearing - BindingContext is not ResultsViewModel");
+		}
 	}
 }
